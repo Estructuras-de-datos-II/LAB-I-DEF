9304e258578f695ef0b1acf40420ae31b2ad21a5
Carlos-Laparra <70443027+Carlos-Laparra@users.noreply.github.com>
2021-09-01 01:06:05 -0600

    Add files via upload

---
+++ Soluciones/Laboratorio 1 EDII/Models/Movies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio_1_EDII.Models
+{
+    public class Movies
+    {
+        public string director { get; set; }
+        public float ratingIMBD { get; set; }
+        public string genero { get; set; }
+        public DateTime lanzamiento { get; set; }
+        public int ratingRottenTomatoes { get; set; }
+        public string titulo { get; set; }
+    }
+}
