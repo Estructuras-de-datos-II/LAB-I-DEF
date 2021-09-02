f03fe6bad8f3c488b2f9af70bda1705fc423b143
martinbechfurlan <martinbechfurlan@gmail.com>
2021-09-01 23:18:16 -0600

    api 2.0

--- Soluciones/Laboratorio 1 EDII/Controllers/MoviesController.cs
+++ Soluciones/Laboratorio 1 EDII/Controllers/MoviesController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 using Laboratorio_1_EDII.Models;
 using Laboratorio_1_EDII.Data;
@@ -14,51 +16,35 @@ namespace Laboratorio_1_EDII.Controllers
     [ApiController]
     public class MoviesController : ControllerBase
     {
-        // GET: api/<MoviesController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-
-
-        // GET api/<MoviesController>/5
-        [HttpGet("{titulo}")]
-        public ActionResult getByString([FromRoute] string Titulo)
-        {
-            var result = Singleton.Instance.Movie.Where(x => x.titulo == Titulo).FirstOrDefault<Movies>();
-            if (result == null) return NotFound();
-            return Ok(result);
-        
-        }
-        // DELETE api/<MoviesController>
-        [HttpDelete]
-        public ActionResult Delete()
-        {
-            Singleton.Instance.Movie.Clear();
-            
-            return NoContent();
-        }
-        //// POST api/<MoviesController>
-        //[HttpPost]
-        //public ActionResult Post([FromBody] Movies newValue)
+        //// GET: api/<MoviesController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
         //{
-        //    try
-        //    {
-        //        var result = Singleton.Instance.Movie.Where(x => x.titulo == newValue.titulo).FirstOrDefault<Movies>();
-        //        if (result != null) return BadRequest();
-
-        //        newValue.id = Singleton.Instance.LastId + 1;
-        //        Singleton.Instance.Movie.Add(newValue);
-        //        Singleton.Instance.LastId++;
-        //        return Created("", newValue);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest();
-        //    }
+        //    return new string[] { "value1", "value2" };
         //}
+
+
+
+        //// GET api/<MoviesController>/5
+        //[HttpGet("{titulo}")]
+        //public ActionResult getByString([FromRoute] string Titulo)
+        //{
+        //    var result = Singleton.Instance.Movie.Where(x => x.titulo == Titulo).FirstOrDefault<Movies>();
+        //    if (result == null) return NotFound();
+        //    return Ok(result);
+        
+        //}
+        //DELETE api/<MoviesController>
+        //[HttpDelete]
+        //public ActionResult Delete()
+        //{
+        //    Singleton.Instance.arbolB.eliminar;
+
+        //    return NoContent();
+        //}
+
+
+
 
         // POST api/<MoviesController>
         [HttpPost]
@@ -67,17 +53,20 @@ namespace Laboratorio_1_EDII.Controllers
             try
             {
                 if (Convert.ToInt32(newValue.Orders) % 2 != 0)
+                {
                     Singleton.Instance.Orders = Convert.ToInt32(newValue.Orders);
+                   return Created("", newValue);
+                }
 
-               
-                else {
+                else
+                {
                     return BadRequest();
                 }
-                return Ok();
+                
             }
             catch (Exception ex)
             {
-                return BadRequest();
+                return BadRequest("NUMERO DEBERÍA DE SER IMPAR");
             }
         }
 
@@ -96,10 +85,41 @@ namespace Laboratorio_1_EDII.Controllers
 
         // POST api/<MoviesController/populate>
 
-        //[HttpPost("populate")]
-        //public async Task<ActionResult> Create([FromForm] IFormFile file)
-        //{
-        //
-        //}
+        [HttpPost("populate")]
+        public async Task<ActionResult> Create([FromForm] IFormFile file)
+        {
+            try
+            {
+
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+
+        }
+
+        // POST api/<MoviesController/import>
+        [HttpPost("import")]
+        public ActionResult Post([FromBody] List<Movies> newListValue)
+        {
+
+            try
+            {
+                foreach (Movies item in newListValue)
+                {
+                    item.id = Singleton.Instance.LastId + 1;
+                    Singleton.Instance.LastId++;
+                    Singleton.Instance.arbolB.insertar(item);
+
+                }
+                return Created("", newListValue);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
     }
 }
